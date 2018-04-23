8dd28744ff38ed37c06a4eb224a0f29249a6156d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/FieldListFront/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+using DevExpress.XtraReports.UserDesigner;
+// ...
+
+namespace FieldListFront {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            // Create a new Design form.
+            XRDesignForm designForm = new XRDesignForm();
+
+            // Obtain the Field List and bring it to front.
+            designForm.DesignDockManager[DesignDockPanelType.FieldList].Show();
+
+            // Create a new blank report.
+            designForm.OpenReport(new XtraReport1());
+
+            // Show the Design form.
+            designForm.ShowDialog();
+        }
+
+    }
+}
---
+++ VB/FieldListFrontVB/Form1.vb
@@ -0,0 +1,22 @@
+﻿Imports System
+Imports System.Windows.Forms
+Imports DevExpress.XtraReports.UserDesigner
+' ...
+
+Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, _
+    ByVal e As System.EventArgs) Handles Button1.Click
+        ' Create a new Design form.
+        Dim designForm As New XRDesignForm()
+
+        ' Obtain the Field List and bring it to front.
+        designForm.DesignDockManager(DesignDockPanelType.FieldList).Show()
+
+        ' Create a new blank report.
+        designForm.OpenReport(New XtraReport1())
+
+        ' Show the Design form.
+        designForm.ShowDialog()
+    End Sub
+End Class
